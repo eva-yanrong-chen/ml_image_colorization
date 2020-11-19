c3535cc6072de2cbbe113cc5fbb81007a8b31dc4
Eric Gu <egu8@gatech.edu>
2020-11-19 17:36:55 -0500

    changed clustering parameters

--- test.py
+++ test.py
@@ -7,28 +7,37 @@ from joblib import dump, load
 def segment_by_annotation(annotation):
     train, val, test = retrieve_data(annotation)
 
-    images = train[0:2]
+    # Shape:
+    # N x H x W x C
+    images = train[:20]
 
-    image = cv2.cvtColor(images[1], cv2.COLOR_LAB2BGR)
+
+    # Segment out light layer:
+    l = images[:, :, :, :1]
+    ab = images[:, :, :, 1:]
+
+    image = cv2.cvtColor(images[0], cv2.COLOR_LAB2BGR)
     cv2.imshow("", image)
     cv2.waitKey(0)
 
     try:
         kmeans = load('weights/{}_kmeans.joblib'.format(annotation))
         print("Loaded Previous parameters")
-        _, pic_changed = cluster_pixels(images,kmeans=kmeans)
+        _, pic_changed = cluster_pixels(images, kmeans=kmeans)
     except:
         print("Could not find old Parameters")
-        kmeans, pic_changed = cluster_pixels(images, K=50)
+        kmeans, pic_changed = cluster_pixels(ab, K=20)
+
 
         dump(kmeans, 'weights/{}_kmeans.joblib'.format(annotation))
+    print("Finished clustering")
     pic_int = np.uint8(pic_changed)
 
-    image = cv2.cvtColor(pic_int[1], cv2.COLOR_LAB2BGR)
+    pic = np.concatenate((l, pic_int), axis=-1)
+
+    image = cv2.cvtColor(pic[0], cv2.COLOR_LAB2BGR)
     cv2.imshow("", image)
     cv2.waitKey(0)
 
-
-
 if __name__  =='__main__':
-    segment_by_annotation('bird')
+    segment_by_annotation('plant_life')
