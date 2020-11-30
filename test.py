ad3becb3cd88db56e964a197b56bcfe6399e0301
Eric Gu <egu8@gatech.edu>
2020-11-30 17:10:25 -0500

    minor changes

--- test.py
+++ test.py
@@ -4,26 +4,30 @@ import cv2
 import numpy as np
 from joblib import dump, load
 
+SIZE = 100
 def segment_by_annotation(annotation):
     train, val, test = retrieve_data(annotation)
 
     # Shape:
     # N x H x W x C
-    images = train[:20]
+    indices = np.random.choice(len(train), SIZE)
+    images = train[indices]
 
 
+    from random import randrange
+    rand_index = randrange(len(images))
     # Segment out light layer:
     l = images[:, :, :, :1]
     ab = images[:, :, :, 1:]
 
-    image = cv2.cvtColor(images[0], cv2.COLOR_LAB2BGR)
+    image = cv2.cvtColor(images[rand_index], cv2.COLOR_LAB2BGR)
     cv2.imshow("", image)
     cv2.waitKey(0)
 
     try:
         kmeans = load('weights/{}_kmeans.joblib'.format(annotation))
         print("Loaded Previous parameters")
-        _, pic_changed = cluster_pixels(images, kmeans=kmeans)
+        _, pic_changed = cluster_pixels(ab, kmeans=kmeans)
     except:
         print("Could not find old Parameters")
         kmeans, pic_changed = cluster_pixels(ab, K=20)
@@ -35,7 +39,7 @@ def segment_by_annotation(annotation):
 
     pic = np.concatenate((l, pic_int), axis=-1)
 
-    image = cv2.cvtColor(pic[0], cv2.COLOR_LAB2BGR)
+    image = cv2.cvtColor(pic[rand_index], cv2.COLOR_LAB2BGR)
     cv2.imshow("", image)
     cv2.waitKey(0)
 
